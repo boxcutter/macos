db8f747b6ee64074d2ef02b2da23421203cb53b9
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-05-31 20:34:14 -0700

    Script to help reduce generated box size
    This script disables hibernation, removes the sleepimage and deletes
    any swapfiles. This reduces uneeded space on disk. Swapfiles will be
    re-genereated as needed. If the box is for VMware, it will also make
    VMware shrink the disk image via VMware tools. On a Yosemite box
    made today this resulted in a reduction from ~14G to ~7G

---
+++ script/minimize.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eux
+
+echo "==> Turning off hibernation"
+pmset hibernatemode 0
+
+echo "==> Getting rid of the sleepimage"
+rm -f /var/vm/sleepimage
+
+echo "==> Stopping the page process and dropping swap files"
+# These will be re-created on boot
+launchctl unload /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist
+sleep 5
+rm -rf /private/var/vm/swap*
+
+# VMware Fusion specific items
+if [ -e .vmfusion_version ] || [[ "$PACKER_BUILDER_TYPE" == vmware* ]]; then
+    # Shrink the disk
+    sudo /Library/Application\ Support/VMware\ Tools/vmware-tools-cli disk shrink /
+fi
