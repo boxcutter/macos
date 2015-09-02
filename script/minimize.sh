2c1c0f840d46a1af408b019c2376cba6c6da2a52
Mitch Tishmack <mitch.tishmack@gmail.com>
2015-09-02 07:30:27 -0700

    Zero free disk space

--- script/minimize.sh
+++ script/minimize.sh
@@ -12,6 +12,10 @@ launchctl unload /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist
 sleep 5
 rm -rf /private/var/vm/swap*
 
+slash="$(df -h / | tail -n 1 | awk '{print $1}')"
+echo Zeroing out free space
+diskutil secureErase freespace 0 ${slash}
+
 # VMware Fusion specific items
 if [ -e .vmfusion_version ] || [[ "$PACKER_BUILDER_TYPE" == vmware* ]]; then
     # Shrink the disk
