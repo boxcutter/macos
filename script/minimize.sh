61094d8cacf8bfd7c7d20cd9484542c29eb9f26e
Petr Baloun <pbaloun@kerio.com>
2015-09-10 16:20:41 +0200

    Disable screensaver

--- script/minimize.sh
+++ script/minimize.sh
@@ -6,6 +6,9 @@ pmset hibernatemode 0
 echo "==> Getting rid of the sleepimage"
 rm -f /var/vm/sleepimage
 
+echo "==> Disabling screensaver"
+defaults -currentHost write com.apple.screensaver idleTime 0
+
 echo "==> Stopping the page process and dropping swap files"
 # These will be re-created on boot
 launchctl unload /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist
