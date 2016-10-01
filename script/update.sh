c8e75a04fe2439487988deaf9e5310d98dee0eeb
Moujan Anna <moujan@annawake.com>
2016-10-01 15:04:41 -0700

    Make update script Sierra-friendly

--- script/update.sh
+++ script/update.sh
@@ -3,7 +3,7 @@
 if [[ "$UPDATE" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
 
     echo "==> Running software update"
-    softwareupdate --install --all -v
+    softwareupdate --install --all -verbose
 
     echo "==> Rebooting the machine"
     reboot
