d1ab4cda204a0689ca667525300ff741bf6e50c5
Moujan Anna <moujan@annawake.com>
2016-10-02 16:19:23 -0700

    Dsiable automatic update check

--- script/update.sh
+++ script/update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 
+echo "==> Disable automatic update check"
+defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticCheckEnabled -bool FALSE
+
 if [[ "$UPDATE" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
 
     echo "==> Running software update"
