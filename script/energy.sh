2b0a68fa49858dba6cc08d6103a43f52caf97e8c
Moujan Anna <moujan@annawake.com>
2016-12-15 23:27:07 -0800

    Dsiable new to mac notification

--- script/energy.sh
+++ script/energy.sh
@@ -4,3 +4,6 @@ echo ==> 'Disabling screensaver'
 defaults -currentHost write com.apple.screensaver idleTime 0
 echo ==> 'Turning off energy saving'
 pmset -a displaysleep 0 disksleep 0 sleep 0
+# https://carlashley.com/2016/10/19/com-apple-touristd/
+echo ==> 'Disable New to Mac notification'
+defaults write com.apple.touristd seed-https://help.apple.com/osx/mac/10.12/whats-new -date "$(date)"
