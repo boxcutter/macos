e5a21b2e5b7f8b8ff95b79650661e72ce6efa7f1
Victor Robertson <vmrob@bittorrent.com>
2017-01-30 20:21:16 -0800

    Fix syntax errors in energy.sh

--- script/energy.sh
+++ script/energy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo ==> 'Disabling screensaver'
+echo "==> 'Disabling screensaver'"
 defaults -currentHost write com.apple.screensaver idleTime 0
-echo ==> 'Turning off energy saving'
+echo "==> 'Turning off energy saving'"
 pmset -a displaysleep 0 disksleep 0 sleep 0
 # https://carlashley.com/2016/10/19/com-apple-touristd/
-echo ==> 'Disable New to Mac notification'
+echo "==> 'Disable New to Mac notification'"
 defaults write com.apple.touristd seed-https://help.apple.com/osx/mac/10.12/whats-new -date "$(date)"
